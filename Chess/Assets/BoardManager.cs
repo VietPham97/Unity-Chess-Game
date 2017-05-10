dc6faa0854f0f9527895225a3b8d6cd0baaa026f
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-05-10 14:13:56 -0400

    Visualize a chessboard on the scene.

---
+++ Chess/Assets/BoardManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BoardManager : MonoBehaviour 
+{
+	private const float TILE_SIZE = 1.0f;
+	private const float TILE_OFFSET = 0.5f;
+
+	private int selectionX = -1; // nothing is selected
+	private int selectionY = -1; // nothing is selected
+
+	private void Update()
+	{
+		DrawChessboard();
+	}
+
+	private void DrawChessboard()
+	{
+		Vector3 widthLine = Vector3.right * 8;
+		Vector3 heigthLine = Vector3.forward * 8;
+
+		for (int i = 0; i <= 8; i++)
+		{
+			Vector3 startPos = Vector3.forward * i;
+			Debug.DrawLine(startPos, startPos + widthLine);
+			for (int j = 0; j <= 8; j++)
+			{
+				startPos = Vector3.right * j;
+				Debug.DrawLine(startPos, startPos + heigthLine);	
+			}	
+		}
+	}
+}
