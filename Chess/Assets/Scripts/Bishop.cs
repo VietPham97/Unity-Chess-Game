aae652980be343659122af785cd374a62d6345a4
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-05-10 23:37:03 -0400

    Implement all possible moves of a single bishop.

--- Chess/Assets/Scripts/Bishop.cs
+++ Chess/Assets/Scripts/Bishop.cs
@@ -1,4 +1,120 @@
 ﻿public class Bishop : Chessman 
 {
+    public override bool[,] PossibleMove()
+    {
+        bool[,] move = new bool[8, 8];
+        
+        Chessman c;
+        int i, j;
 
+        // Diagonal Left Up
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i--;
+            j++;
+            
+            if (i < 0 || j > 7) break; // Outside of the board
+
+            c = BoardManager.Instance.Chessman [i, j];
+
+            if (c == null)
+            {
+                move [i, j] = true;
+            }
+            else
+            {
+                if (isWhite != c.isWhite)
+                {
+                    move [i, j] = true;
+                }
+
+                break;
+            }
+        }
+
+        // Diagonal Right Up
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i++;
+            j++;
+            
+            if (i > 7 || j > 7) break; // Outside of the board
+
+            c = BoardManager.Instance.Chessman [i, j];
+
+            if (c == null)
+            {
+                move [i, j] = true;
+            }
+            else
+            {
+                if (isWhite != c.isWhite)
+                {
+                    move [i, j] = true;
+                }
+
+                break;
+            }
+        }
+
+        // Diagonal Left Down
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i--;
+            j--;
+            
+            if (i < 0 || j < 0) break; // Outside of the board
+
+            c = BoardManager.Instance.Chessman [i, j];
+
+            if (c == null)
+            {
+                move [i, j] = true;
+            }
+            else
+            {
+                if (isWhite != c.isWhite)
+                {
+                    move [i, j] = true;
+                }
+
+                break;
+            }
+        }
+
+        // Diagonal Right Down
+        i = CurrentX;
+        j = CurrentY;
+        while (true)
+        {
+            i++;
+            j--;
+            
+            if (i > 7 || j < 0) break; // Outside of the board
+
+            c = BoardManager.Instance.Chessman [i, j];
+
+            if (c == null)
+            {
+                move [i, j] = true;
+            }
+            else
+            {
+                if (isWhite != c.isWhite)
+                {
+                    move [i, j] = true;
+                }
+
+                break;
+            }
+        }
+    
+        return move;
+    }
 }
