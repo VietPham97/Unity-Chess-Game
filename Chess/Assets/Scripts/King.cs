f97cce536f3742b7623b77a68304d35f20ade903
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-05-10 23:48:23 -0400

    Implement all possible moves of a single king.

--- Chess/Assets/Scripts/King.cs
+++ Chess/Assets/Scripts/King.cs
@@ -1,4 +1,94 @@
 ﻿public class King : Chessman 
 {
+    public override bool[,] PossibleMove()
+    {   
+        bool[,] move = new bool[8, 8];
 
+        Chessman c;
+        int i, j;
+
+        // Up, Diagonal Left, Diagonal Right
+        i = CurrentX - 1;
+        j = CurrentY + 1;
+
+        if (CurrentY != 7) /* Top side of the board from White view */
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (i >= 0 || i <= 7) /* Inside the board */
+                {
+                    c = BoardManager.Instance.Chessman [i, j];
+
+                    if (c == null) /* empty space */
+                    {
+                        move [i, j] = true;
+                    }
+                    else if (isWhite != c.isWhite) /* there is an enemy */
+                    {
+                        move [i, j] = true;
+                    }
+                }
+                
+                i++;
+            }
+        }
+
+        // Left
+        if (CurrentX != 0)
+        {
+            c = BoardManager.Instance.Chessman [CurrentX - 1, CurrentY];
+
+            if (c == null) /* empty space */
+            {
+                move [CurrentX - 1, CurrentY] = true;
+            }
+            else if (isWhite != c.isWhite) /* there is an enemy */
+            {
+                move [CurrentX - 1, CurrentY] = true;
+            }
+        }
+        
+        // Right
+        if (CurrentX != 7)
+        {
+            c = BoardManager.Instance.Chessman [CurrentX + 1, CurrentY];
+
+            if (c == null) /* empty space */
+            {
+                move [CurrentX + 1, CurrentY] = true;
+            }
+            else if (isWhite != c.isWhite) /* there is an enemy */
+            {
+                move [CurrentX + 1, CurrentY] = true;
+            }
+        }
+        
+        // Down, Diagonal Left, Diagonal Right
+        i = CurrentX - 1;
+        j = CurrentY - 1;
+
+        if (CurrentY != 0) /* Bottom side of the board from White view */
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (i >= 0 || i <= 7) /* Inside the board */
+                {
+                    c = BoardManager.Instance.Chessman [i, j];
+
+                    if (c == null) /* empty space */
+                    {
+                        move [i, j] = true;
+                    }
+                    else if (isWhite != c.isWhite) /* there is an enemy */
+                    {
+                        move [i, j] = true;
+                    }
+                }
+
+                i++;
+            }
+        }
+
+        return move;
+    }
 }
