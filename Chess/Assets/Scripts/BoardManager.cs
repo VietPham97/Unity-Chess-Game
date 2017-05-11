cad2a3dd2da180255beeda1b7ba92f461bed8fe2
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-05-10 21:30:36 -0400

    Switch turn after 1 move.

--- Chess/Assets/Scripts/BoardManager.cs
+++ Chess/Assets/Scripts/BoardManager.cs
@@ -60,6 +60,7 @@ public class BoardManager : MonoBehaviour
 			Chessman[selectedChessman.CurrentX, selectedChessman.CurrentY] = null; // set the start position to null
 			selectedChessman.transform.position = GetTileCenter(x, y); // move that selected chessman to the new position
 			Chessman[x, y] = selectedChessman; // store new position value in the script
+			isWhiteTurn = !isWhiteTurn;
 		}
 
 		selectedChessman = null; // make player unselect his/her chessman
