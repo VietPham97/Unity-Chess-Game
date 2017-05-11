d7eb9b983469c310575833486a39a24326b2b167
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-05-10 23:09:04 -0400

    Implement all possible moves of a single rook.

--- Chess/Assets/Scripts/Rook.cs
+++ Chess/Assets/Scripts/Rook.cs
@@ -1,4 +1,100 @@
 ﻿public class Rook : Chessman 
 {
+    public override bool[,] PossibleMove()
+    {
+        bool[,] move = new bool[8, 8];
+        
+        Chessman c;
+        int i;
 
+        // Right
+        i = CurrentX;
+        while (true)
+        {
+            i++;
+            if (i > 7) break; // outside the board
+
+            c = BoardManager.Instance.Chessman [i, CurrentY];
+            
+            if (c == null)
+            {
+                move [i, CurrentY] = true;
+            }
+            else
+            {
+                if (c.isWhite != isWhite) // there is an enemy
+                    move [i, CurrentY] = true;
+
+                break;
+            }
+        }
+
+        // Left
+        i = CurrentX;
+        while (true)
+        {
+            i--;
+            if (i < 0) break; // outside the board
+
+            c = BoardManager.Instance.Chessman [i, CurrentY];
+            
+            if (c == null)
+            {
+                move [i, CurrentY] = true;
+            }
+            else
+            {
+                if (c.isWhite != isWhite) // there is an enemy
+                    move [i, CurrentY] = true;
+
+                break;
+            }
+        }
+
+        // Up
+        i = CurrentY;
+        while (true)
+        {
+            i++;
+            if (i > 7) break; // outside the board
+
+            c = BoardManager.Instance.Chessman [CurrentX, i];
+            
+            if (c == null)
+            {
+                move [CurrentX, i] = true;
+            }
+            else
+            {
+                if (c.isWhite != isWhite) // there is an enemy
+                    move [CurrentX, i] = true;
+
+                break;
+            }
+        }
+
+        // Down
+        i = CurrentY;
+        while (true)
+        {
+            i--;
+            if (i < 0) break; // outside the board
+
+            c = BoardManager.Instance.Chessman [CurrentX, i];
+            
+            if (c == null)
+            {
+                move [CurrentX, i] = true;
+            }
+            else
+            {
+                if (c.isWhite != isWhite) // there is an enemy
+                    move [CurrentX, i] = true;
+
+                break;
+            }
+        }
+
+        return move;
+    }
 }
